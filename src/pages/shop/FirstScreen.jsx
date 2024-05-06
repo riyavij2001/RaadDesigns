f0986578bc2c42815cf20861bb4993b565fddebb
riyavij2001 <riyavij2001@gmail.com>
2024-05-06 14:14:39 +0530

    Added Products for Homedecor

--- src/pages/shop/FirstScreen.jsx
+++ src/pages/shop/FirstScreen.jsx
@@ -8,41 +8,54 @@ const FirstScreen = () => {
 
     const HomeDecor = [
         {
-            title: "ABC",
-            imageSrc: "/static/images/Pic1.jpg"
+            title: "Elysian Glow",
+            imageSrc: "/static/images/HomeDecorItem1.jpeg"
         },
         {
-            title: "ABC",
-            imageSrc: "/static/images/Pic1.jpg"
+            title: "Tri Lumina",
+            imageSrc: "/static/images/HomeDecorItem2.jpeg"
         },
         {
-            title: "ABC",
-            imageSrc: "/static/images/Pic1.jpg"
+            title: "Orbit Ease",
+            imageSrc: "/static/images/HomeDecorItem3.jpeg"
         },
         {
-            title: "ABC",
-            imageSrc: "/static/images/Pic1.jpg"
+            title: "Aurora Aura",
+            imageSrc: "/static/images/HomeDecorItem4.jpeg"
         },
         {
-            title: "ABC",
-            imageSrc: "/static/images/Pic1.jpg"
+            title: "Golden Mystery",
+            imageSrc: "/static/images/HomeDecorItem5.jpeg"
         },
         {
-            title: "ABC",
-            imageSrc: "/static/images/Pic1.jpg"
+            title: "Timeless Trove",
+            imageSrc: "/static/images/HomeDecorItem6.jpeg"
         },
         {
-            title: "ABC",
-            imageSrc: "/static/images/Pic1.jpg"
+            title: "Gleaming Gaze",
+            imageSrc: "/static/images/HomeDecorItem7.jpeg"
         },
         {
-            title: "ABC",
-            imageSrc: "/static/images/Pic1.jpg"
+            title: "Celestial Canvas",
+            imageSrc: "/static/images/HomeDecorItem8.jpeg"
         },
         {
-            title: "ABC",
-            imageSrc: "/static/images/Pic1.jpg"
-        }
+            title: "Earthy Elegance",
+            imageSrc: "/static/images/HomeDecorItem9.jpeg"
+        },
+        {
+            title: "Elegantease",
+            imageSrc: "/static/images/HomeDecorItem10.jpeg"
+        },
+        {
+            title: "Central Chic",
+            imageSrc: "/static/images/HomeDecorItem11.jpeg"
+        },
+        {
+            title: "Botanical Bloom",
+            imageSrc: "/static/images/HomeDecorItem12.jpeg"
+        },
+        
     ]
 
     return (
@@ -102,7 +115,7 @@ const FirstScreen = () => {
                             </tbody>
                         </table>
                     </div>
-                    <div className=' w-[160vh] grid grid-cols-3 gap-9 '>
+                    <div className=' w-[140vh] grid grid-cols-3 gap-8 '>
                         {HomeDecor.map((items) => {
                             return <div className=" card w-80 h-[20em] bg-base-100 shadow-xl">
                                 <figure><img src={items.imageSrc} alt="Shoes" className=' rounded-b-xl' /></figure>
